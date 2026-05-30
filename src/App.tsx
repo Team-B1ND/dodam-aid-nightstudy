51f94c8cdf10be34d80d6b43b3ea2ef9ef6736a3
gituser <danjee2009@gmail.com>
2026-05-30 14:13:58 +0900

    fix: 잘못된 오버레이 프로바이더 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BridgeProvider } from "@b1nd/aid-kit/bridge-kit/web";
 import { SafeAreaProvider } from "@b1nd/aid-kit/safe-area-provider";
 import { AppStateProvider } from "@b1nd/aid-kit/app-state";
 import { RouteProvider, Router } from "@b1nd/aid-kit/navigation";
-import { OverlayProvider } from "@b1nd/dodam-design-system/components";
+// import { OverlayProvider } from "@b1nd/dodam-design-system/components";
 import NightStudyPage from "./pages";
 
 
@@ -18,9 +18,9 @@ const App = () => (
     <SafeAreaProvider>
       <AppStateProvider>
         <RouteProvider routes={routes}>
-          <OverlayProvider>
+          {/*<OverlayProvider>*/}
             <Router routes={routes} />
-          </OverlayProvider>
+          {/*</OverlayProvider>*/}
         </RouteProvider>
       </AppStateProvider>
     </SafeAreaProvider>
