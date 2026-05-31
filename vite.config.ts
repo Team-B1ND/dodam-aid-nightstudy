f0af6e52c7d13b0fe88a6c56df7ff391aaf1f801
김찬우 <chanwoo219@gmail.com>
2026-05-31 12:07:46 +0900

    chore: api 프록시 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/nightstudy': {
+        target: 'https://dodam-api.b1nd.com',
+        changeOrigin: true,
+        secure: false,
+        cookieDomainRewrite: 'localhost',
+      },
+      '/auth': {
+        target: 'https://dodam-api.b1nd.com',
+        changeOrigin: true,
+        secure: false,
+        cookieDomainRewrite: 'localhost',
+      },
+      '/user': {
+        target: 'https://dodam-api.b1nd.com',
+        changeOrigin: true,
+        secure: false,
+        cookieDomainRewrite: 'localhost',
+      },
+    }
+  }
 })
