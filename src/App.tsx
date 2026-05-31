683e135ac7d510ff4a7ab4034f4132cacd4a79dc
김찬우 <chanwoo219@gmail.com>
2026-05-31 12:10:32 +0900

    feat: 다크/라이트 테마 적용 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
 import { BridgeProvider } from "@b1nd/aid-kit/bridge-kit/web";
 import { SafeAreaProvider } from "@b1nd/aid-kit/safe-area-provider";
-import { AppStateProvider } from "@b1nd/aid-kit/app-state";
+import { AppStateProvider, useAppState } from "@b1nd/aid-kit/app-state";
 import { RouteProvider, Router } from "@b1nd/aid-kit/navigation";
 // import { OverlayProvider } from "@b1nd/dodam-design-system/components";
 import NightStudyPage from "./pages";
-
+import { useEffect } from "react";
 
 const routes = {
-  tabs: [
-    { path: "/", index: true, element: NightStudyPage }
-  ],
-  stacks: []
+    tabs: [
+        { path: "/", index: true, element: NightStudyPage }
+    ],
+    stacks: []
+};
+
+const ThemeApplier = () => {
+    const [theme] = useAppState<"light" | "dark">("dark", "theme");
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme);
+    }, [theme]);
+
+    return null;
 };
 
 const App = () => (
-  <BridgeProvider>
-    <SafeAreaProvider>
-      <AppStateProvider>
-        <RouteProvider routes={routes}>
-          {/*<OverlayProvider>*/}
-            <Router routes={routes} />
-          {/*</OverlayProvider>*/}
-        </RouteProvider>
-      </AppStateProvider>
-    </SafeAreaProvider>
-  </BridgeProvider>
+    <BridgeProvider>
+        <SafeAreaProvider>
+            <AppStateProvider>
+                <ThemeApplier />
+                <RouteProvider routes={routes}>
+                    {/*<OverlayProvider>*/}
+                    <Router routes={routes} />
+                    {/*</OverlayProvider>*/}
+                </RouteProvider>
+            </AppStateProvider>
+        </SafeAreaProvider>
+    </BridgeProvider>
 );
 
-export default App
+export default App;
